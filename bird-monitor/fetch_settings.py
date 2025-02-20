8bf8d46b3ca45a78a2017004081086f648049b4b
Alfie Prettyman <or22503@bristol.ac.uk>
2025-02-20 11:25:57 +0000

    edit fetch settings

--- bird-monitor/fetch_settings.py
+++ bird-monitor/fetch_settings.py
@@ -13,7 +13,7 @@ client_list = ['pi@192.168.0.101',
                'pi@192.168.0.100']
 client_password = 'whatever' # password for client
 client_data_path = '/home/pi/Data/' # path to data on client
-server_data_path = '/home/pi/Downloads/Data/' # path where data should go on server
+server_data_path = '/home/pi/Downloads/birdcam/Data/' # path where data should go on server
 fetch_then_move = False # trigger move2ext.py straight after finishing with fetching (something is off with this, so leave it False and call it separately)
 ext_paths = ['/mnt/chickenHDD1/Data/','/mnt/chickenHDD2/Data'] # list of external paths
 ext_checks = ['/mnt/chickenHDD1/Data/hello.check','/mnt/chickenHDD2/Data/hello.check'] # list of files to check if the external drive is mounted
